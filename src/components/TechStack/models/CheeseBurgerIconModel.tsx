be37ceedad9e1a6de8f59f828d2bf3849394f355
leota <leotagit@gmail.com>
2024-04-14 09:42:22 +0100

    ✨ Add camera animation and user interaction to TechStack component

--- src/components/TechStack/models/CheeseBurgerIconModel.tsx
+++ src/components/TechStack/models/CheeseBurgerIconModel.tsx
@@ -1,11 +1,38 @@
-import { FC } from "react";
-
+import { FC, useCallback, useRef } from "react";
 import { useGLTF } from "@react-three/drei";
+import { ThreeEvent } from "@react-three/fiber";
+import * as THREE from "three";
 
-export const CheeseBurgerIconModel: FC = () => {
+type Props = {
+  onClick: () => void;
+};
+
+export const CheeseBurgerIconModel: FC<Props> = ({ onClick }) => {
+  const ref = useRef<THREE.Object3D>(null!);
   const { scene } = useGLTF("/assets/cheeseburger-icon.glb");
 
-  return <primitive object={scene} />;
+  const onPointerOver = useCallback((e: ThreeEvent<PointerEvent>) => {
+    e.stopPropagation();
+    document.body.style.cursor = "pointer";
+    ref.current.position.z = 0.2;
+    ref.current.updateMatrix();
+
+  }, []);
+
+  const onPointerOut = useCallback(() => {
+    document.body.style.cursor = "default";
+    ref.current.position.z = 0;
+  }, []);
+
+  return (
+    <primitive
+      ref={ref}
+      object={scene}
+      onPointerOver={onPointerOver}
+      onPointerOut={onPointerOut}
+      onClick={onClick}
+    />
+  );
 };
 
 useGLTF.preload("/assets/cheeseburger-icon.glb");
