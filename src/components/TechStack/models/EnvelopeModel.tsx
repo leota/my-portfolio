324565460deb99fdc0a04ba2ad22f3692a79b8fa
leota <leotagit@gmail.com>
2024-04-20 11:44:53 +0100

    ✨ Add burger items animation

--- src/components/TechStack/models/EnvelopeModel.tsx
+++ src/components/TechStack/models/EnvelopeModel.tsx
@@ -1,11 +1,41 @@
-import { FC } from "react";
+import { FC, useEffect, useRef, useState } from "react";
+import * as THREE from "three";
 
-import { useGLTF } from "@react-three/drei";
+import { useAnimations, useGLTF } from "@react-three/drei";
 
-export const EnvelopeModel: FC = () => {
-  const { scene } = useGLTF("/assets/envelope.glb");
+type Props = {
+  isPlaying: boolean;
+};
 
-  return <primitive object={scene} />;
+export const EnvelopeModel: FC<Props> = ({ isPlaying }) => {
+  const [play, setPlay] = useState(isPlaying);
+  const groupRef = useRef<THREE.Group>(null!);
+  const { scene, animations } = useGLTF("/assets/envelope.glb");
+  const { actions } = useAnimations(animations, groupRef);
+
+  useEffect(() => {
+    setPlay(isPlaying);
+  }, [isPlaying]);
+
+  useEffect(() => {
+    const actionNames = Object.keys(actions);
+    if (actionNames.length > 0 && play) {
+      actionNames.forEach((name) => {
+        const action = actions[name];
+        if (action) {
+          action.clampWhenFinished = true;
+          action.setLoop(THREE.LoopOnce, 1);
+          action.play();
+        }
+      });
+    }
+  }, [actions, play]);
+
+  return (
+    <group ref={groupRef}>
+      <primitive object={scene} />
+    </group>
+  );
 };
 
 useGLTF.preload("/assets/envelope.glb");
