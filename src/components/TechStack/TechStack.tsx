b74d00267c88d475a3278a4e61014e1b73d45f73
leota <leotagit@gmail.com>
2024-04-21 13:54:08 +0100

    ✨ Add pizza items animation

--- src/components/TechStack/TechStack.tsx
+++ src/components/TechStack/TechStack.tsx
@@ -8,6 +8,7 @@ import { CheeseBurgerIconModel } from "./models/CheeseBurgerIconModel";
 import { CameraAnimation } from "./models/CameraAnimation";
 import { DocumentsModel } from "./models/DocumentsModel";
 import { BurgerItemsModel } from "./models/BurgerItemsModel";
+import { PizzaItemsModel } from "./models/PizzaItemsModel";
 
 const baseXPosition = 0;
 const baseYPosition = 0;
@@ -18,23 +19,32 @@ let position: THREE.Vector3;
 
 export const TechStack: FC = () => {
   const [isPlaying, setPlaying] = useState(false);
+  const [isPizzaPlaying, setPizzaPlaying] = useState(false);
+  const [isBurgerPlaying, setBurgerPlaying] = useState(false);
   const ref = useRef<THREE.Group>(null!);
   position = new THREE.Vector3(baseXPosition, baseYPosition, 0);
 
-  const handleClick = () => {
-    setPlaying(true);
-  };
+ const handlePizzaClick = () => {
+    setPlaying(!isPlaying);
+    setPizzaPlaying(!isPizzaPlaying);
+  }
+
+  const handleBurgerClick = () => {
+    setPlaying(!isPlaying);
+    setBurgerPlaying(!isBurgerPlaying);
+  }
 
   return (
     <group ref={ref} position={position}>
       <CameraAnimation isPlaying={isPlaying} />
       <StructureModel />
       <UIPanelModel />
-      <PizzaIconModel onClick={handleClick} />
-      <CheeseBurgerIconModel onClick={handleClick} />
+      <PizzaIconModel onClick={handlePizzaClick} />
+      <CheeseBurgerIconModel onClick={handleBurgerClick} />
       <EnvelopeModel isPlaying={isPlaying} />
       <DocumentsModel isPlaying={isPlaying} />
-      <BurgerItemsModel isPlaying={isPlaying} />
+      {isBurgerPlaying && <BurgerItemsModel isPlaying={isBurgerPlaying} />}
+      {isPizzaPlaying && <PizzaItemsModel isPlaying={isPizzaPlaying} />}
     </group>
   );
 };
