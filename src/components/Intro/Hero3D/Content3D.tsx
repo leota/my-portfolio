1a13fc9fe0f9889f37c66d2884552da7e23ccf92
leota <leotagit@gmail.com>
2024-04-23 13:41:37 +0100

    💄 Add rotation animation to Content3D component and hide horizontal overflow in globals.css

--- src/components/Intro/Hero3D/Content3D.tsx
+++ src/components/Intro/Hero3D/Content3D.tsx
@@ -1,11 +1,19 @@
-import { FC } from "react";
+import { FC, useRef } from "react";
+import * as THREE from "three";
 
 import { RoomModel } from "./RoomModel";
 import { TypingModel } from "./TypingModel";
+import { useFrame } from "@react-three/fiber";
 
 export const Content3D: FC = () => {
+  const ref = useRef<THREE.Group>(null!);
+
+  useFrame(() => {
+    ref.current.rotation.y += 0.001;
+  });
+
   return (
-    <group rotation={[0, -30, 0]} scale={0.7}>
+    <group ref={ref} rotation={[0, -30, 0]} scale={0.7}>
       <RoomModel />
       <TypingModel />
     </group>
