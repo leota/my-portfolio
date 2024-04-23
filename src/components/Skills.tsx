43b7e8f4eda5c2ba3bd0d52c402236196557214c
leota <leotagit@gmail.com>
2024-04-23 17:48:58 +0100

    💄 Refactor Skills component styles

--- src/components/Skills.tsx
+++ src/components/Skills.tsx
@@ -19,8 +19,8 @@ export const Image: FC<{
 );
 
 export const Skills: FC = () => (
-  <div className="relative flex flex-col  w-full  items-center justify-start xl:py-16">
-    <div className="absolute top-0 left-0 w-full h-full bg-white bg-opacity-10 rounded-lg" />
+  <div className="relative flex flex-col w-full py-4 items-center justify-start xl:py-16">
+    <div className="absolute top-0 left-0 w-full h-full bg-blue-400 bg-opacity-20 rounded-lg" />
     <h3 className="text-2xl text-gray-50 font-extrabold m-4 lg:text-3xl xl:text-5xl xl:mb-8">
       My Skills
     </h3>
