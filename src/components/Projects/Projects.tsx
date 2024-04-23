9d2b9275a23ae2aacab78c9be3d71d6d80dc7b59
leota <leotagit@gmail.com>
2024-04-23 21:54:26 +0100

    ✨ Polygona project added

--- src/components/Projects/Projects.tsx
+++ src/components/Projects/Projects.tsx
@@ -1,8 +1,6 @@
 import { FC } from "react";
 import { Project } from "./Project";
 
-
-
 export const Projects: FC = () => (
   <div className="relative flex flex-col w-full py-4 items-center justify-start xl:py-16 my-4">
     <h3 className="h3 text-gray-50 font-extrabold mb-8 lg:text-3xl xl:text-5xl xl:mb-32">
@@ -10,30 +8,36 @@ export const Projects: FC = () => (
     </h3>
     <div className="grid grid-cols-1 gap-4 justify-items-center items-center md:gap-16 xl:gap-32">
       <Project
+        title="Polygona"
+        image="/images/polygona.png"
+        alt="housecure"
+        url="https://polygona.io"
+      />
+      <Project
         title="Housecure"
         image="/images/housecure-cover.png"
         alt="housecure"
         url="https://housecure.co.uk"
+        reverse
       />
       <Project
         title="Views For Change"
         image="/images/vfc.png"
         alt="housecure"
         url="https://www.facebook.com/viewsforchange"
-        reverse
       />
       <Project
         title="AllPlants"
         image="/images/allplants.png"
         alt="housecure"
         url="https://allplants.com"
+        reverse
       />
       <Project
         title="Doctorlink"
         image="/images/doctorlink.png"
         alt="housecure"
         url="https://www.doctorlink.com"
-        reverse
       />
     </div>
   </div>
