1b55a663c31a0b1cf23eecb182f0afb5c5a65171
leota <leotagit@gmail.com>
2024-04-27 08:01:41 +0100

    🔧 Update Content3D component with responsive scaling

--- src/components/Hero/Hero3D/Content3D.tsx
+++ src/components/Hero/Hero3D/Content3D.tsx
@@ -4,16 +4,19 @@ import * as THREE from "three";
 import { RoomModel } from "./RoomModel";
 import { TypingModel } from "./TypingModel";
 import { useFrame } from "@react-three/fiber";
+import { useViewport } from "@/hooks/use-viewport";
 
 export const Content3D: FC = () => {
   const ref = useRef<THREE.Group>(null!);
+  const { isDesktop, isLargeDesktop } = useViewport();
+  const scale = (isDesktop || isLargeDesktop) ? 0.75 : 1;
 
   useFrame(() => {
     ref.current.rotation.y += 0.001;
   });
 
   return (
-    <group ref={ref} rotation={[0, -30, 0]} scale={0.7}>
+    <group ref={ref} rotation={[0, -30, 0]} scale={scale}>
       <ambientLight intensity={1} color={0xffacac} />
       <RoomModel />
       <TypingModel />
