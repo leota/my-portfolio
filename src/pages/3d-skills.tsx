651b2c93b895d11e36851a74016c8d98e5ad360c
leota <leotagit@gmail.com>
2024-04-27 08:41:35 +0100

    ♻️ Refactor LoadingScreen component to use useState hook in LoadingScreen.tsx

--- src/pages/3d-skills.tsx
+++ src/pages/3d-skills.tsx
@@ -1,24 +1,14 @@
-import Head from "next/head";
-
-import { Suspense, useState } from "react";
 import { LoadingScreen } from "@/components/LoadingScreen";
 import { TechStack } from "@/components/TechStack/TechStack";
+import { NextSeo } from "next-seo";
 
 export default function Skills3DTour() {
-  const [isLoading, setIsLoading] = useState(true);
-
   return (
     <>
-      <Head>
-        <title>Skills 3D tour</title>
-        <meta name="viewport" content="width=device-width, initial-scale=1" />
-        <link rel="icon" href="/favicon.ico" />
-      </Head>
+      <NextSeo title="3D Skills" />
       <main className="h-screen">
-        <LoadingScreen isLoading={isLoading} setIsLoading={setIsLoading} />
-        <Suspense fallback={null}>
-          <TechStack />
-        </Suspense>
+        <LoadingScreen />
+        <TechStack />
       </main>
     </>
   );
